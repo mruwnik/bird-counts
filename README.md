5cadd7c74d08538a63a6a530cdf0dd391cd7680f
Daniel O'Connell <tojad99@gmail.com>
2022-01-08 21:46:34 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bird counter
+
+A simple web app that simulates birds singing to each other, with the option of reacting to a different birds singing. Counts can be downloaded - reload the page to restart the simulation.
+
+There is also the possibility of simulating observers (i.e. researchers trying to count birds) which can move around (or not) on the basis of various counting methods. Observers can also be dragged around to manually position them.
+
+## Demo
+
+Check [here](https://media.ahiru.pl/birds/) to see a working version.
+
+## Getting started
+
+Shadow-cljs is used for compiling, so make sure it's [available](https://shadow-cljs.github.io/docs/UsersGuide.html#_command_line). If so, then `shadow-cljs server` should be enough to get things running.
+I personally use Emacs + Cider for development, but a quick test of the above allowed me to run things locally.
--- test/birds/core_test.cljs
+++ test/birds/core_test.cljs
@@ -1,17 +1,18 @@
 (ns birds.core-test
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
-            [birds.core :as sut]))
+            [birds.actors :as actors]
+            [birds.bird :as suta]))
 
-(let [bird (sut/->Bird (atom {:pos {:x 100 :y 100}
-                              :volume 50
-                              :audio-sensitivity 100}) nil)]
+(let [bird (sut/map->Bird {:pos {:x 100 :y 100}
+                           :volume 50
+                           :audio-sensitivity 100})]
   ;; out of range
-  (not (sut/hears? bird {:pos {:x 1000 :y 1000} :volume 10}))
-  (not (sut/hears? bird {:pos {:x 1000 :y -1000} :volume 10}))
-  (not (sut/hears? bird {:pos {:x 100 :y 210} :volume 10}))
-  (not (sut/hears? bird {:pos {:x 100 :y 240} :volume 40}))
-  (not (sut/hears? bird {:pos {:x 200 :y 200} :volume 41}))
+  (not (actors/hears? bird {:pos {:x 1000 :y 1000} :volume 10}))
+  (not (actors/hears? bird {:pos {:x 1000 :y -1000} :volume 10}))
+  (not (actors/hears? bird {:pos {:x 100 :y 210} :volume 10}))
+  (not (actors/hears? bird {:pos {:x 100 :y 240} :volume 40}))
+  (not (actors/hears? bird {:pos {:x 200 :y 200} :volume 41}))
 
   ;; edge of range
-  (sut/hears? bird {:pos {:x 200 :y 200} :volume 42})
-  (sut/hears? bird {:pos {:x 100 :y 219} :volume 20}))
+  (actors/hears? bird {:pos {:x 200 :y 200} :volume 42})
+  (actors/hears? bird {:pos {:x 100 :y 219} :volume 20}))
